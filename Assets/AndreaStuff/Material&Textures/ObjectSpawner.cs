c78b3d884d44725f11357a1faf5bbee0f5694d06
andreayatek96 <andrea.cicero.studio.96@gmail.com>
2023-03-04 14:28:00 +0000

    Don't break my stuff

---
+++ Assets/AndreaStuff/Material&Textures/ObjectSpawner.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject[] objectList;
+    public int numObjects;
+    public float sphereRadius;
+    public float objectScale;
+    public float maxVelocity;
+    public float minVelocity;
+
+    void Start()
+    {
+        for (int i = 0; i < numObjects; i++)
+        {
+            GameObject newObj = Instantiate(objectList[Random.Range(0, objectList.Length)], Random.insideUnitSphere * sphereRadius + transform.position, Quaternion.identity);
+            newObj.transform.localScale *= Random.Range(0.5f, 1.5f) * objectScale;
+            newObj.transform.parent = transform;
+            Rigidbody rb = newObj.AddComponent<Rigidbody>();
+            rb.useGravity = false;
+            MeshCollider mc = newObj.AddComponent<MeshCollider>();
+            rb.velocity = Random.insideUnitSphere * Random.Range(minVelocity, maxVelocity);
+            rb.angularVelocity = Vector3.zero;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sphereRadius);
+    }
+}
