1ee45ea17c645d08dc75c9b324526086cdff12cd
AnasMations <anas.ahassan@yahoo.com>
2023-03-04 16:35:05 +0200

    Change Render Pipeline

--- Assets/Scripts/SpaceLocomotion.cs
+++ Assets/Scripts/SpaceLocomotion.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class SpaceLocomotion : MonoBehaviour
 {
+    public bool isGrapping = false;
     public Transform rightHand;
     Vector3 lastRightHandPosition;
 
-    void Update()
+    void Start()
     {
-        Vector3 movementVector = rightHand.position - lastRightHandPosition;
-        transform.position += movementVector;
         lastRightHandPosition = rightHand.position;
+    }
 
-        GetComponent<Rigidbody>().velocity = movementVector/Time.deltaTime;
+    void FixedUpdate()
+    {
+        if(rightHand.position == Vector3.zero)
+        {
+            isGrapping = false;
+            Debug.Log("Right hand is not tracked");
+        }
+        else
+        {
+            isGrapping = true;
+            Debug.Log("Right hand is tracked");
+        }
+
+        if(isGrapping)
+        {
+            Vector3 movementVector = rightHand.position - lastRightHandPosition;
+            transform.position += movementVector;
+            lastRightHandPosition = rightHand.position;
+            
+            //GetComponent<Rigidbody>().velocity = movementVector/Time.deltaTime;
+        }
+
     }
 }
