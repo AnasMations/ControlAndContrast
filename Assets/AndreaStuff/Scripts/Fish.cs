b790c1f0439bbc9f7524e6a6fc37fdc2b7309c3c
andreayatek96 <andrea.cicero.studio.96@gmail.com>
2023-03-08 21:59:19 +0000

    Underwater content kinda complete

---
+++ Assets/AndreaStuff/Scripts/Fish.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Fish : MonoBehaviour
+{
+    public Transform followTarget; // the object the fish will follow
+    public float speed = 5f; // the speed at which the fish will follow
+    public float acceleration = 2f; // the acceleration of the fish
+
+    private Vector3 velocity; // the velocity of the fish
+
+    void Update()
+    {
+        // calculate the direction to the follow target
+        Vector3 direction = followTarget.position - transform.position;
+
+        // calculate the desired velocity based on speed and direction
+        Vector3 desiredVelocity = direction.normalized * speed;
+
+        // calculate the change in velocity based on acceleration
+        Vector3 deltaVelocity = Vector3.ClampMagnitude(desiredVelocity - velocity, acceleration * Time.deltaTime);
+
+        // update the velocity
+        velocity += deltaVelocity;
+
+        // move the fish based on the velocity
+        transform.position += velocity * Time.deltaTime;
+
+        // rotate the fish to face the follow target
+        if (velocity.magnitude > 0.1f)
+        {
+            transform.rotation = Quaternion.LookRotation(velocity, transform.up);
+        }
+    }
+}
+    
